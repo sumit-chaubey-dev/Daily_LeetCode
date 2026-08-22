88b4dda23b66bb35468d3bf6774155d2199d4b6b
Sumit Chaubey <sumitchaubey191@gmail.com>
2026-08-22 23:57:32 +0530

    Day 04: Array problems

---
+++ DAY_04/448_find_all_numbers_disappeared_in_an_array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        ans = []
+        nums_set = set(nums)
+
+        for i in range(1, len(nums) + 1):
+            if i not in nums_set:
+                ans.append(i)
+
+        return ans
